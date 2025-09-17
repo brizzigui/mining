a0aaca91ab83ed64c392f905b90c49efc1dec07e
Mathbvb <mr.mathiasbvb@gmail.com>
2025-09-17 10:48:03 -0300

    feat: implemented data preprocessing function

--- trabalhos/t1/clean.py
+++ trabalhos/t1/clean.py
@@ -1,7 +1,37 @@
 import pandas as pd
 
-def read() -> pd.DataFrame:
-    pass
+def read() -> pd.DataFrame:    
+    data = []
+    
+    with open('kendo_matches_work.csv', 'r') as file:
+        categories = file.readline().strip().split(",")
+        for line in file:
+            values = line.strip().split(",")
+
+            if (len(values) != len(categories)):
+                continue
+
+            row = dict(zip(categories, values))
+
+            try:
+                summed = sum(int(values[i]) for i in range(5, 9))
+                time = int(row["seconds_between"])
+                ippons = int(row["ippon_taken"])
+            except (ValueError, IndexError):
+                continue
+
+            if summed != 1 or time < 0 or time > 100 or ippons not in (0, 1):
+                continue
+
+            data.append(row)
+    
+    df = pd.DataFrame(data, columns=[
+        "match_id", "ippon_number", "seconds_between", "ippon_taken", "men", "kote", "do", "tsuki"
+    ])
+    df = df.drop_duplicates()
+    df.to_csv('cleaned.csv', index=False)
+    
+    return df
 
 def mine(data: pd.DataFrame) -> pd.DataFrame:
     pass
