a20ce86dfa19df4c85eaf23356c92843f962a869
Guilherme Brizzi <brizzigui@gmail.com>
2025-09-18 13:26:56 -0300

    feat: q2 and q3 calculations

--- trabalhos/t1/clean.py
+++ trabalhos/t1/clean.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from mlxtend.frequent_patterns import apriori, association_rules
 
 def verify(row: dict) -> bool:
     try:
@@ -14,7 +15,7 @@ def verify(row: dict) -> bool:
     
     return True
 
-def read() -> pd.DataFrame:   
+def read() -> dict:   
     data = []
     match_id = None
     match_count = 2
@@ -37,29 +38,122 @@ def read() -> pd.DataFrame:
 
                 if match_count > 0:
                     if match_id != row["match_id"]:
-                        val = {f"{key}_1": row[key] for key in useful_categories}
+                        val = {f"{key}_1": int(row[key]) for key in useful_categories}
                         match_id = row["match_id"] 
 
                     else:
-                        row = {f"{key}_2": row[key] for key in useful_categories}
+                        row = {f"{key}_2": int(row[key]) for key in useful_categories}
                         data.append(val | row) 
 
                     match_count -= 1
 
-    
     df = pd.DataFrame(data, columns=[
         *data[0].keys()
     ])
     df.to_csv('cleaned.csv', index=False)
-    
-    return df
 
-def mine(data: pd.DataFrame) -> pd.DataFrame:
-    pass
+    return data
+
+def attack_implies_in_not(data: dict) -> pd.DataFrame:
+    lhs = ["men_1", "kote_1", "do_1", "tsuki_1"]
+    rhs = ["men_2", "kote_2", "do_2", "tsuki_2"]
+
+    df = [
+            {key: bool(val[key]) if i < 4 else not bool(val[key]) for i, key in 
+                enumerate(lhs + rhs)
+            } 
+            for val in data
+        ]
+    df = pd.DataFrame(df, columns=[
+        *df[0].keys()
+    ])
+
+
+    sets = apriori(df, min_support=0.01, use_colnames=True)   
+    rules = association_rules(sets, metric="confidence", min_threshold=0.9).sort_values(by="support", ascending=False)
+    rules = rules[
+        (rules['antecedents'].apply(len) == 1) &
+        (rules['consequents'].apply(len) == 1)
+    ]
+
+
+    rules = rules[
+        rules['antecedents'].apply(lambda x: list(x)[0] in lhs) &
+        rules['consequents'].apply(lambda x: list(x)[0] in rhs)
+    ]
+
+    print()
+    print("Strongest rule a->!b in attacks:\n(highest support, above threshold confidence)\n")
+    print(rules.iloc[0])
+    print()
+
+    return rules
+
+def men_implies_ippon(data: dict) -> float:
+    firsts = ["men_1", "ippon_taken_1"]
+    seconds = ["men_2", "ippon_taken_2"]
+    netral_notation = ["men", "ippon_taken"]
+
+    df = []
+    for val in data:
+        df.append({nkey: bool(val[key]) for nkey, key in zip(netral_notation, firsts)})
+        df.append({nkey: bool(val[key]) for nkey, key in zip(netral_notation, seconds)})
+
+    df = pd.DataFrame(df, columns=[
+        *df[0].keys()
+    ])
+
+    sets = apriori(df, min_support=0.01, use_colnames=True)   
+    rules = association_rules(sets, metric="confidence", min_threshold=0.01).sort_values(by="support", ascending=False)
+
+    print(rules)
+
+    return rules.iat[0, 5]
+
+def slow_attacks(data: dict) -> pd.DataFrame:
+    lhs = ["men_2", "kote_2", "do_2", "tsuki_2"]
+    rhs = ["over25"]
+
+    df = []
+    for val in data:
+        df.append({key: bool(val[key]) for key in lhs} |
+                  {"over25": bool(val["seconds_between_2"] > 25)})
+
+    df = pd.DataFrame(df, columns=[
+        *df[0].keys()
+    ])
+
+    sets = apriori(df, min_support=0.01, use_colnames=True)   
+    rules = association_rules(sets, metric="confidence", min_threshold=0.85).sort_values(by="support", ascending=False)
+
+    rules = rules[
+        (rules['antecedents'].apply(len) == 1) &
+        (rules['consequents'].apply(len) == 1)
+    ]
+
+    rules = rules[
+        rules['antecedents'].apply(lambda x: list(x)[0] in lhs) &
+        rules['consequents'].apply(lambda x: list(x)[0] in rhs)
+    ]
+
+    print(rules)
+
+
+def mine(data: dict) -> None:
+    # for question 1
+    attack_implies_in_not(data)
+
+    # for question 2
+    print(f"Confidence = {men_implies_ippon(data)}")
+
+    # for question 3
+    slow_attacks(data)
+
+    # for question 4
 
 def main() -> None:
-    df = read()
-    result = mine(df)
+    data = read()
+    mine(data)
 
 if __name__ == "__main__":
     main()
